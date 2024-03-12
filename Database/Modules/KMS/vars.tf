50257b9cfe11afaacd7b19aac97f12a84cc20ed9
tavisca-abhishek-srivastava <Abhishek.Srivastava@tavisca.com>
2024-03-12 16:29:50 +0530

    Changed directory structure for Examples

---
+++ Database/Modules/KMS/vars.tf
@@ -0,0 +1,19 @@
+variable "kms_alias" {
+    description = "define in the form of 'alias/unique_key_name'"
+    type = string
+}
+
+variable "delete_after_days" {
+    description = " The waiting period, specified in number of days. After the waiting period ends, AWS KMS deletes the KMS key.it must be between 7 and 30, inclusive"
+    type = number
+}
+
+variable "key_description" {
+    description = "The description of the key as visible in AWS console"
+    type = string
+}
+variable "key_policy_map" {
+    description = "A valid policy JSON document"
+    type = any
+ 
+}
