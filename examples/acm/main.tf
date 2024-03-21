a7d9c5a421c4eac71e1b10cea6cbf64d0b516fea
Tavisca-shruti-joshi <shruti.joshi@tavisca.com>
2024-03-21 10:40:41 +0530

    adding acm example

---
+++ examples/acm/main.tf
@@ -0,0 +1,26 @@
+module acm {
+  source  = "../../modules/acm"
+  domain_name = "wwwpoc.net"
+  zone_id     = "Z04304331OGX97SQM2XED"
+
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+      "*.wwwpoc.net",
+      "test.wwwpoc.net"
+  ]
+
+  tags = {
+    Name               = "test-acm-sre"
+    Example            = "complete"
+    AppName            = "test-acm"
+    Backup             = "no"
+    BusinessUnit       = "travel.poc"
+    DataClassification = "internal"
+    Environment        = "poc"
+    InfraOwner         = "sre-cloud-reliability@tavisca.com"
+    Name               = "IaaC module for Provisioning"
+    Product            = "sre"
+
+  }
+}
