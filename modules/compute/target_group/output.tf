9d3c7c91a2548a0d56b8f3ad478a68668cf783e9
Aryan Gupta <e912920@cxloyaltycorp.com>
2024-05-08 11:24:28 +0530

    added tfvars for alb with listener and target group example

--- modules/compute/target_group/output.tf
+++ modules/compute/target_group/output.tf
@@ -1,6 +1,6 @@
 output "default_target_group_arn" {
   description = "The default target group ARN"
-  value       = one(aws_lb_target_group.default[*].arn)
+  value       = aws_lb_target_group.default.arn
 }
 
 output "default_target_group_arn_suffix" {
