05bdebc19edbb3dfe4fcdcab8eb4fc7465ec4892
Aryan Gupta <e912920@cxloyaltycorp.com>
2024-05-08 11:33:49 +0530

    bug fix

--- modules/compute/target_group/output.tf
+++ modules/compute/target_group/output.tf
@@ -1,6 +1,6 @@
 output "default_target_group_arn" {
   description = "The default target group ARN"
-  value       = aws_lb_target_group.default.arn
+  value       = one(aws_lb_target_group.default[*].arn)
 }
 
 output "default_target_group_arn_suffix" {
